fce7ff3e267d138c93dc4cd0e7d3fbcfa6e9bfd8
rida <elbahtouri0123@gmail.com>
2020-10-16 14:26:54 +0100

    set up files and install necessary pakages

---
+++ src/component/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from './nav'
+function App() {
+  return (
+    <div className="App">
+       <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Nav=()=> {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>LOg in</li>
+            </ul>
+        </div>
+    )
+}
+export default Nav
