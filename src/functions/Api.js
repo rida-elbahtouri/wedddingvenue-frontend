00a43140557429cf9cf2cda23962a23ee8f96110
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-26 11:24:26 +0100

    fix api methods to hundle jwt

--- src/functions/Api.js
+++ src/functions/Api.js
@@ -5,12 +5,17 @@ export const GetVenuesApi = () => Axios.get(`${baseUrl}/weddingvenues`);
 
 export const AddTofavourite = (token, VenueId) => {
   Axios.post(`${baseUrl}/favourites`, {
-    token,
     weddingvenue_id: VenueId,
+  }, {
+    headers: {
+      Authorization: token,
+    },
   });
 };
 
 export const GetDetailsApi = id => Axios.get(`${baseUrl}/weddingvenues/${id}`);
 export const GetfavouritesApi = token => Axios.get(`${baseUrl}/favourites`, {
-  params: { token },
+  headers: {
+    Authorization: token,
+  },
 });
